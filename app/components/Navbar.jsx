d3f378667f262061e22ebeef9af3427aa6c7ab8c
Ginodavecte <ginojoanroy@hotmail.com>
2025-02-01 23:38:43 +0100

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4">
+        <a href="">
+          <Image src={assets.logo} alt="hek" className="w-28 cursor-pointer mr-14"/>
+        </a>
+
+        <ul>
+          <li><a href="#top">Top</a></li>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#work">My Work</a></li>
+          <li><a href="#contact">Contact me</a></li>
+        </ul>
+
+        <div>
+          <a href="#contact">Contact <Image src={assets.arrow_icon} className="w-3" alt="henk"/></a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
